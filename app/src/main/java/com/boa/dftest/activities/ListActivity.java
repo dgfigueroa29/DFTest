c7e1545f563ac915938dfe60b932295001f64c7e
David Figueroa <dgfigueroa29@gmail.com>
2017-03-30 18:52:59 -0300

    Add assets and activities templates

---
+++ app/src/main/java/com/boa/dftest/activities/ListActivity.java
@@ -0,0 +1,16 @@
+package com.boa.dftest.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.boa.dftest.R;
+
+public class ListActivity extends AppCompatActivity
+{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_list);
+	}
+}
---
+++ app/src/main/java/com/boa/dftest/utils/Api.java
@@ -0,0 +1,9 @@
+package com.boa.dftest.utils;
+
+/**
+ * Created by David on 30/03/2017.
+ */
+
+public class Api
+{
+}
